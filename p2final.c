0088ae8b6ce7062dbce35d03dbf29f63235a7d54
Pallavi1105 <94678026+Pallavi1105@users.noreply.github.com>
2022-02-25 09:51:42 +0000

    p2final

--- p2final.c
+++ p2final.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("enter the values");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  float area;
+  if(area>0)
+    return 1;
+  return 0;
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
+  if(is_triangle)
+  printf("three lines form triangle");
+  else 
+  printf("three lines do not form triangle");
+}
+int main()
+{
+  float x1,y1,x2,y2,x3,y3;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  int 
+  istriangle=is_triangle(x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,istriangle);
+  return 0; 
+}
 
